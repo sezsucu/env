36747350f31aa56c0ac3366ac28c1393083629d0
Sezgin <noemail_please@gmail.com>
2018-10-31 03:13:08 +0000

    largeDirs

--- bin/largeDirs.sh
+++ bin/largeDirs.sh
@@ -2,8 +2,8 @@
 
 # A script to show the largest directories in the given directory or the current directory
 # The size of a directory is computed such that only its direct files
-# are considered, not its sub-directories.
-
+# are considered, not its sub-directories. This is quite easy to do on linux
+# using 'du -S . | sort -nr | head -15' but on mac it is not that easy.
 
 source "$ENV_HOME_DIR/bash/lib.sh"
 
@@ -23,47 +23,49 @@ if [[ "$path" =~ .*\/$ ]]; then
     path=${path:0:$len}
 fi
 
-sizes=(0 0 0 0 0 0 0 0 0)
-names=('' '' '' '' '' '' '' '' '' '')
+declare -a sizes
+declare -a names
+limit=10
 function computeDir()
 {
     local path=$1
     local totalSize=0
     local fileSize=0
-    #echo $path
     totalSize=$(find "$path" -type f -maxdepth 1 -print0 | xargs -0 stat -f%z | awk '{b+=$1} END {print b}')
-#    for file in $path/*; do
-#        if [ -f "$file" ]; then
-#            if [ "$ENV_PLATFORM" = "Mac" ]; then
-#                set -- $(\stat -f%z "$file")
-#                fileSize=$1
-#            else
-#                set -- $(\stat -c%z "$file")
-#                fileSize=$1
-#            fi
-#            ((totalSize+=fileSize))
-#        fi
-#    done
-    #dirs=$(find "$path" -type d -maxdepth 1 -print0 | xargs -0 stat -f%z | awk '{b+=$1} END {print b}')
-    #for file in $path/*; do
-    for file in $(find "$path" -type d -maxdepth 1 -print0 | xargs -0 echo); do
+    local i
+    if [[ $totalSize -gt 0 ]]; then
+        if [[ ${#sizes[@]} -lt $limit ]]; then
+            sizes+=($totalSize)
+            names+=("$totalSize $path")
+        else
+            for (( i=0 ; i < ${#sizes[@]} ; i++ )); do
+                if [[ ${sizes[$i]} -lt $totalSize ]]; then
+                    sizes[$i]=$totalSize
+                    names[$i]="$totalSize $path"
+                    i=100000
+                fi
+            done
+        fi
+    fi
+
+    local filesStr=$(find "$path" -type d -maxdepth 1)
+    local files
+    IFS=$'\n' read -rd '' -a files <<<"$filesStr"
+    for (( i=0 ; i < ${#files[@]} ; i++ )); do
+        local file=${files[$i]}
         if [[ "$path" != "$file" ]]; then
             if [ -d "$file" ]; then
                 computeDir "$file"
             fi
         fi
     done
-    for (( i=0 ; i < ${#sizes[@]} ; i++ )); do
-        if [[ ${sizes[$i]} -lt $totalSize ]]; then
-            sizes[$i]=$totalSize
-            names[$i]="$totalSize $path"
-            i=100000
-        fi
-    done
 }
+
+
 
 echo $path
 computeDir "$path"
+
 IFS=$'\n' sorted=($(sort -rn <<<"${names[*]}"))
 
 for (( i=0 ; i < ${#sizes[@]} ; i++ )); do
