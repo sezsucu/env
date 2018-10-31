f333c2a4d5f716a4041f16da46e5aca7b4c9ae4f
Sezgin <noemail_please@gmail.com>
2018-10-31 15:45:54 +0000

    dirTree better and handles spaces

--- bin/dirTree.sh
+++ bin/dirTree.sh
@@ -20,6 +20,7 @@ fi
 
 function showLevel()
 {
+    local i
     for (( i=0 ; i < $1 ; i++ )); do
         printf "."
     done
@@ -30,22 +31,28 @@ function showDir()
     local path=$1
     local level=$2
     local k=0
-    for file in $path/*; do
-        if [ -d "$file" ]; then
+    local filesStr=$(find "$path" -type d -maxdepth 1)
+    local files
+    local file
+    local i
+    IFS=$'\n' read -rd '' -a files <<<"$filesStr"
+    for (( i=0 ; i < ${#files[@]} ; i++ )); do
+        file=${files[$i]}
+        if [[ "$file" != "$path" && -d "$file" ]]; then
             showLevel $level
             if (( k > 0 )); then
-                echo '|' `basename $file`
+                echo '|' $(basename "$file")
             else
-                echo '\' `basename $file`
+                echo '\' $(basename "$file")
             fi
-            showDir $file $((level+1))
+            showDir "$file" $((level+1))
             ((k++))
         fi
     done
 }
 
-echo $path
-showDir $path 1
+echo "$path"
+showDir "$path" 1
 
 
 
