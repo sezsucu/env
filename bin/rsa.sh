0313e00a159d9898a66e6a4f6c709ea3fcebcb13
Sezgin Sucu <no-reply@gmail.com>
2018-11-07 16:11:54 +0000

    encryption

--- bin/rsa.sh
+++ bin/rsa.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://www.zimuel.it/blog/sign-and-verify-a-file-using-openssl
+
 function getFileName()
 {
     keyFile=$1
@@ -36,20 +38,71 @@ function extractPublicRSAKey()
 function createRSAKey()
 {
     keyFile=$(getFileName "$1")
-    openssl genrsa -out $keyFile 3072
+    openssl genrsa -out $keyFile 4096
     chmod 600 $keyFile
     echo "Generated $keyFile"
+}
+
+function encryptData()
+{
+    keyFile=$(getFileName "$1")
+    (>&2 echo "Using $keyFile")
+    read -r line < "$keyFile"
+    if [[ "$line" =~ .*PRIVATE.* ]]; then
+        openssl rsautl -inkey $keyFile -encrypt >&1
+    elif [[ "$line" =~ .*PUBLIC.* ]]; then
+        openssl rsautl -inkey $keyFile -pubin -encrypt >&1
+    fi
+}
+
+function decryptData()
+{
+    keyFile=$(getFileName "$1")
+    (>&2 echo "Using $keyFile")
+    openssl rsautl -inkey $keyFile -decrypt <&0
+}
+
+function signData()
+{
+    keyFile=$(getFileName "$1")
+    (>&2 echo "Using $keyFile")
+    openssl dgst -sha256 -sign $keyFile <&0
+}
+
+function verifyData()
+{
+    keyFile=$(getFileName "$1")
+    (>&2 echo "Using $keyFile")
+    openssl dgst -sha256 -verify $keyFile -signature <&0
 }
 
 command=$1
 shift
 
 case "$command" in
-    create)
+    cre*)
         createRSAKey "$*"
         ;;
-    public)
+
+    pub*)
         extractPublicRSAKey "$*"
         ;;
 
+    enc*)
+        encryptData "$*"
+        ;;
+
+    dec*)
+        decryptData "$*"
+        ;;
+
+     sig*)
+        signData "$*"
+        ;;
+
+     ver*)
+        verifyData "$*"
+        ;;
+    *)
+        echo "rsa.sh [(cre)ate|(pub)lic|(enc)rypt|(dec)rypt] fileName"
 esac
