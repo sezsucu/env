657dc30c4ac7248218a726df119415a039da2679
Sezgin Sucu <no-reply@gmail.com>
2018-11-29 05:09:11 +0000

    managing vars

---
+++ bin/manageVars.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+function showHelp ()
+{
+    echo "manageVars.sh [-a varName varValue][-h][-d varName]"
+    echo "-a varName: add or update varName with the varValue"
+    echo "-d varName: delete the varName"
+    echo "-l: lists all the varNames and their values"
+    exit 0
+}
+
+function addVar ()
+{
+    local name=$1
+    local value=$2
+    if [[ -e $ENV_DATA_DIR/bash/customVars.sh && -n "$(grep ${name}= $ENV_DATA_DIR/bash/customVars.sh)" ]]; then
+        echo "Found $name in $ENV_DATA_DIR/bash/customVars.sh, updating its value"
+        sed -i".bak" "/export $name=/d" $ENV_DATA_DIR/bash/customVars.sh
+    fi
+    echo "export $name='$value'" >> $ENV_DATA_DIR/bash/customVars.sh
+}
+
+function removeVar ()
+{
+    local name=$1
+    if [[ -e $ENV_DATA_DIR/bash/customVars.sh && -n "$(grep ${name}= $ENV_DATA_DIR/bash/customVars.sh)" ]]; then
+        echo "Removed $name in $ENV_DATA_DIR/bash/customVars.sh"
+        sed -i".bak" "/export $name=/d" $ENV_DATA_DIR/bash/customVars.sh
+    else
+        echo "No such var: $name"
+    fi
+}
+
+function listVars ()
+{
+    if [ -e $ENV_DATA_DIR/bash/customVars.sh ]; then
+        cat $ENV_DATA_DIR/bash/customVars.sh
+    else
+        echo "Empty List"
+    fi
+}
+
+varName=""
+add=0
+while getopts "h?a:d:l" opt; do
+    case "$opt" in
+    h|\?)
+        showHelp
+        ;;
+    a)
+        varName=$OPTARG
+        add=1
+        ;;
+    d)
+        varName=$OPTARG
+        add=0
+        ;;
+    l)
+        listVars
+        exit 0
+        ;;
+    esac
+done
+shift $(expr $OPTIND - 1 )
+
+if [[ $add == 1 ]]; then
+    if [[ $# == 0 ]]; then
+        echo "Missing varValue"
+        exit 1
+    fi
+    varValue=$1
+    addVar $varName "$varValue"
+else
+    removeVar $varName
+fi
+
+
